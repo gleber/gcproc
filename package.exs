d25f250aabdea30e7d8727198c19d2832c3a05ea
Gleb Peregud <gleber.p@gmail.com>
2012-11-22 11:05:45 +0100

    add expm manifest - v0.5.0

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "gcproc", description: "Garbage collected processes",
+                 version: "0.5.0", keywords: ["gc", "process"], 
+                 maintainers: [[name: "Gleb Peregud", 
+                                email: "gleber.p@gmail.com"]],
+                 repositories: [[github: "gleber/gcproc"]])
+    
